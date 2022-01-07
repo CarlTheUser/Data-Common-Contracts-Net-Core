1df6a78f96e9d43cc8e21eeefc275e46ea2584bb
carlandres18@gmail.com <carlandres18@gmail.com>
2022-01-07 21:31:07 +0800

    Added IEventStore interface

---
+++ Data.Common.Contracts/IEventStore.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Data.Common.Contracts
+{
+    public class DataEvent
+    {
+
+    }
+
+    public interface IEventStore
+    {
+        IEnumerable<DataEvent> ReleaseEvents();
+    }
+}
