e657a49c273b8ccaec4e1600dfad171378bac42e
carlandres18@gmail.com <carlandres18@gmail.com>
2022-01-13 13:13:57 +0800

    Repository.Find returns nullable and Repository.Save to not accept nulls in parameter

--- Data.Common.Contracts/IRepository.cs
+++ Data.Common.Contracts/IRepository.cs
@@ -5,13 +5,13 @@ namespace Data.Common.Contracts
 {
     public interface IRepository<TKey, TItem>
     {
-        TItem Find(TKey key);
+        TItem? Find(TKey key);
         void Save(TItem item);
     }
 
     public interface IAsyncRepository<TKey, TItem>
     {
-        Task<TItem> FindAsync(TKey key, CancellationToken token);
+        Task<TItem?> FindAsync(TKey key, CancellationToken token);
         Task SaveAsync(TItem item, CancellationToken token);
     }
 }
