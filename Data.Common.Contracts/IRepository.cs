dbfa5aa3c42d350a84930bc0f5356e8c7dccd141
carlandres18@gmail.com <carlandres18@gmail.com>
2022-05-21 12:33:40 +0800

    Added more IRepository interfaces

--- Data.Common.Contracts/IRepository.cs
+++ Data.Common.Contracts/IRepository.cs
@@ -14,4 +14,22 @@ namespace Data.Common.Contracts
         Task<TItem?> FindAsync(TKey key, CancellationToken token);
         Task SaveAsync(TItem item, CancellationToken token);
     }
+
+    //Template for specific implementations: SqlSpec(string ToSqlClase()), GenericSpec(bool IsSatisfiedBy(condition))... etc.
+    public interface ISpecification
+    {
+
+    }
+
+    public interface IRepository<TItem>
+    {
+        TItem? Find(ISpecification specs);
+        void Save(TItem item);
+    }
+
+    public interface IAsyncRepository<TItem>
+    {
+        Task<TItem?> FindAsync(ISpecification specs, CancellationToken token);
+        Task SaveAsync(TItem item, CancellationToken token);
+    }
 }
